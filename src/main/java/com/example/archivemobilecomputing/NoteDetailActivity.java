4030ee9ad71b4a88490218456cbddc3c2e1dcd75
Mgc77 <57840741+Mgc77@users.noreply.github.com>
2022-12-24 21:45:05 +0000

    Add files via upload

---
+++ src/main/java/com/example/archivemobilecomputing/NoteDetailActivity.java
@@ -0,0 +1,180 @@
+package com.example.archivemobilecomputing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+
+import java.util.Date;
+import java.util.Calendar;
+
+public class NoteDetailActivity extends AppCompatActivity
+{
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
+    private EditText descEditText;
+    private Button deleteButton;
+    private Note selectedNote;
+    private Button titleEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_detail);
+        initWidgets();
+        initDatePicker();
+        dateButton = findViewById(R.id.titleEditText);
+        dateButton.setText(getTodaysDate());
+        checkForEditNote();
+    }
+
+    private void initWidgets()
+    {
+        titleEditText = findViewById(R.id.titleEditText);
+        descEditText = findViewById(R.id.descriptionEditText);
+        deleteButton = findViewById(R.id.deleteNoteButton);
+    }
+
+    private void checkForEditNote()
+    {
+        Intent previousIntent = getIntent();
+
+        int passedNoteID = previousIntent.getIntExtra(Note.NOTE_EDIT_EXTRA, -1);
+        selectedNote = Note.getNoteForID(passedNoteID);
+
+        if (selectedNote != null)
+        {
+            titleEditText.setText(selectedNote.getTitle());
+            descEditText.setText(selectedNote.getDescription());
+        }
+        else
+        {
+            deleteButton.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void saveNote(View view)
+    {
+        SQLiteManager sqLiteManager = SQLiteManager.instanceOfDatabase(this);
+        String title = String.valueOf(titleEditText.getText());
+        String desc = String.valueOf(descEditText.getText());
+
+        if(selectedNote == null)
+        {
+            int id = Note.noteArrayList.size();
+            Note newNote = new Note(id, title, desc);
+            Note.noteArrayList.add(newNote);
+            sqLiteManager.addNoteToDatabase(newNote);
+        }
+        else
+        {
+            selectedNote.setTitle(title);
+            selectedNote.setDescription(desc);
+            sqLiteManager.updateNoteInDB(selectedNote);
+        }
+
+        finish();
+    }
+
+    public void deleteNote(View view)
+    {
+        selectedNote.setDeleted(new Date());
+        SQLiteManager sqLiteManager = SQLiteManager.instanceOfDatabase(this);
+        sqLiteManager.updateNoteInDB(selectedNote);
+        finish();
+    }
+
+    private String getTodaysDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+
+
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        //datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+    }
+
+
+    private String makeDateString(int day, int month, int year)
+    {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+
+    private String getMonthFormat(int month)
+    {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+        //default should never happen
+        return "JAN";
+    }
+
+
+
+
+
+
+
+
+    public void openDatePicker(View view)
+    {
+        datePickerDialog.show();
+    }
+}
